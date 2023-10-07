414b4c82b3a78f6df293827459b913d184b57e18
Kacper <109245559+kacpi-k@users.noreply.github.com>
2023-10-07 15:24:51 +0200

    Coś zostało zrobione

---
+++ lib/views/home_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        //backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Center(child: Text('Mobilny Informator'),
+        ),
+        
+      ),
+      body: Column(
+        children: [
+          Align(
+            alignment: Alignment.center,
+            child: Image.asset('icons/herb_icon.png'),
+          ),
+          const Text('Urząd Miasta Płock'),
+        ],
+      ),
+    );
+  }
+}
