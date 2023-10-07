150a0ae962a53c3b90685e9c7af7fe6e5e242e2e
Kacper <109245559+kacpi-k@users.noreply.github.com>
2023-10-07 16:59:38 +0200

    home page mod

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:u_nas_dziala_project/constants/colors.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,9 +14,15 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         //backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Center(child: Text('Mobilny Informator'),
+        title: const Center(
+          child: Text('Mobilny Informator'),
         ),
-        
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.settings),
+          )
+        ],
       ),
       body: Column(
         children: [
@@ -24,6 +31,73 @@ class _HomePageState extends State<HomePage> {
             child: Image.asset('icons/herb_icon.png'),
           ),
           const Text('Urząd Miasta Płock'),
+          const SizedBox(
+            height: 100,
+          ),
+          Row(
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                margin: const EdgeInsets.only(left: 57),
+                decoration: BoxDecoration(
+                  color: greyCustomColor,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Center(
+                  child: Image.asset('icons/sprawa_button_icon.png'),
+                ),
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                margin: const EdgeInsets.only(left: 45),
+                decoration: BoxDecoration(
+                  color: greyCustomColor,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Center(
+                  child: Image.asset('icons/pomoc_button_icon.png'),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(left: 63),
+                child: const Text('test'),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                margin: const EdgeInsets.only(left: 57, top: 100),
+                decoration: BoxDecoration(
+                  color: greyCustomColor,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Center(
+                  child: Image.asset('icons/kontakt_button_icon.png'),
+                ),
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                margin: const EdgeInsets.only(left: 45, top: 100),
+                decoration: BoxDecoration(
+                  color: greyCustomColor,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Center(
+                  child: Image.asset('icons/informacje_button_icon.png'),
+                ),
+              ),
+            ],
+          )
         ],
       ),
     );
