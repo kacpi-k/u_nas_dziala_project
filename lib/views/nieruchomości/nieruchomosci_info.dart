8162bb7b2ac9795a5d6e92be5dc6214045f3eaa1
kacperpasikowski <pasikowskik@wp.pl>
2023-10-07 22:15:03 +0200

    push

---
+++ lib/views/nieruchomości/nieruchomosci_info.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+class NieruchomoscInfo extends StatefulWidget {
+  const NieruchomoscInfo({super.key});
+
+  @override
+  State<NieruchomoscInfo> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<NieruchomoscInfo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        //backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Center(
+          child: Text('Mobilny Informator'),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.center,
+              child: Image.asset('icons/herb_icon.png'),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 21,
+              ),
+              'Wszystkie formularze, które muszą zostać wypełnione aby móc opłacić podatek od posiadanej nieruchomości znajdziesz w zakładce “Podatek od nieruchomości” pod przyciskiem “Dokumenty do pobrania”.',
+            ),
+            const SizedBox(
+              height: 220,
+            ),
+            Column(
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    // Tutaj możesz dodać akcję, która ma być wykonywana po naciśnięciu przycisku.
+                    // Na przykład można dodać nawigację do innej strony.
+                  },
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(350, 50),
+                    backgroundColor: Color.fromARGB(192, 43, 206, 46),
+                    padding: EdgeInsets.all(16.0), // Dostosuj wcięcie przycisku
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(
+                          8.0), // Dostosuj zaokrąglenie rogów
+                      side: const BorderSide(
+                          color: Color.fromARGB(
+                              192, 43, 193, 193)), // Dostosuj kolor obramowania
+                    ),
+                  ),
+                  child: const Text(
+                    'Podatek od nieruchomości IN-1',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 255, 255, 255),
+                      fontSize: 18.0, // Dostosuj rozmiar tekstu
+                      fontWeight: FontWeight.bold, // Dostosuj wagę tekstu
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // Tutaj możesz dodać akcję, która ma być wykonywana po naciśnięciu przycisku.
+                    // Na przykład można dodać nawigację do innej strony.
+                  },
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(350, 50),
+                    backgroundColor: Color.fromARGB(204, 50, 64, 255),
+                    padding: EdgeInsets.all(16.0), // Dostosuj wcięcie przycisku
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(
+                          8.0), // Dostosuj zaokrąglenie rogów
+                      side: const BorderSide(
+                          color: Color.fromARGB(
+                              192, 3, 100, 100)), // Dostosuj kolor obramowania
+                    ),
+                  ),
+                  child: const Text(
+                    'Pomoc',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 255, 255, 255),
+                      fontSize: 18.0, // Dostosuj rozmiar tekstu
+                      fontWeight: FontWeight.bold, // Dostosuj wagę tekstu
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
