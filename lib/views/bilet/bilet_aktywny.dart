e67233bfad60dae4201a2a121c55261fe52774fa
kacperpasikowski <pasikowskik@wp.pl>
2023-10-08 11:40:34 +0200

    jojo

--- lib/views/bilet/bilet_aktywny.dart
+++ lib/views/bilet/bilet_aktywny.dart
@@ -109,124 +109,127 @@ class _QueuePageState extends State<QueuePage> {
           centerTitle: true,
           title: const Text('Twój bilet'),
         ),
-        body: Column(children: [
-          const SizedBox(
-            height: 30,
-          ),
-          Row(
-            children: [
-              Container(
-                margin: const EdgeInsets.only(left: 24),
-                width: 125.0,
-                height: 125.0,
-                decoration: BoxDecoration(
-                  color: const Color.fromARGB(192, 43, 206, 46),
-                  borderRadius: BorderRadius.circular(120.0),
-                ),
-                child: const Center(
-                  child: Text(
-                    '46',
-                    style: TextStyle(fontSize: 50),
+        body: SingleChildScrollView(
+          child: Column(children: [
+            const SizedBox(
+              height: 30,
+            ),
+            Row(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(left: 24),
+                  width: 125.0,
+                  height: 125.0,
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(192, 43, 206, 46),
+                    borderRadius: BorderRadius.circular(120.0),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      '46',
+                      style: TextStyle(fontSize: 50),
+                    ),
                   ),
                 ),
-              ),
-              Container(
-                margin: const EdgeInsets.only(right: 24, left: 50),
-                child: const Text(
-                  'Twój numer\nbiletu',
-                  style: TextStyle(fontSize: 16),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          Row(
-            children: [
-              Container(
-                margin: const EdgeInsets.only(left: 24),
-                width: 125.0,
-                height: 125.0,
-                decoration: BoxDecoration(
-                  color: const Color.fromARGB(192, 255, 0, 0),
-                  borderRadius: BorderRadius.circular(120.0),
-                ),
-                child: Center(
-                  child: Text(
-                    '$currentQueuePosition',
-                    style: const TextStyle(fontSize: 50),
+                Container(
+                  margin: const EdgeInsets.only(right: 24, left: 50),
+                  child: const Text(
+                    'Twój numer\nbiletu',
+                    style: TextStyle(fontSize: 16),
+                    textAlign: TextAlign.center,
                   ),
                 ),
-              ),
-              Container(
-                margin: const EdgeInsets.only(right: 24, left: 40),
-                child: const Text(
-                  'Twoja obecna\npozycja w kolejce',
-                  style: TextStyle(fontSize: 16),
-                  textAlign: TextAlign.center,
+              ],
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Row(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(left: 24),
+                  width: 125.0,
+                  height: 125.0,
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(192, 255, 0, 0),
+                    borderRadius: BorderRadius.circular(120.0),
+                  ),
+                  child: Center(
+                    child: Text(
+                      '$currentQueuePosition',
+                      style: const TextStyle(fontSize: 50),
+                    ),
+                  ),
                 ),
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          const Center(
-            child: Text(
-              'Powiadomimy Cię, gdy liczba osób\nprzed tobą zmniejszy się do 3.',
-              textAlign: TextAlign.center,
+                Container(
+                  margin: const EdgeInsets.only(right: 24, left: 40),
+                  child: const Text(
+                    'Twoja obecna\npozycja w kolejce',
+                    style: TextStyle(fontSize: 16),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          const Center(
-            child: Text(
-              'Udaj się do:\nUrząd Miasta Płocka- Oddział Komunikacji\nAleja marszałka Józefa Piłsudzkiego 6\ntel. 24 364 55 55',
-              textAlign: TextAlign.center,
+            const SizedBox(
+              height: 30,
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          const Center(
+            const Center(
               child: Text(
-            'Twoją sprawą zajmię się:\nJolanta Krysiak Pokój\nnr. 26 (wejście od parkingu)\ntel. +48 909 789 675\nE-mail: jkrysiak87@plock.eu',
-            textAlign: TextAlign.center,
-          )),
-          const SizedBox(
-            height: 20,
-          ),
-          ElevatedButton(
-            onPressed: () {
-              // Tutaj możesz dodać akcję, która ma być wykonywana po naciśnięciu przycisku.
-              // Na przykład można dodać nawigację do innej strony.
-              Navigator.of(context)
-                  .pushNamedAndRemoveUntil(homePage, (route) => false);
-            },
-            style: ElevatedButton.styleFrom(
-              minimumSize: const Size(350, 50),
-              backgroundColor: const Color.fromARGB(173, 244, 18, 18),
-              padding: const EdgeInsets.all(16.0), // Dostosuj wcięcie przycisku
-              shape: RoundedRectangleBorder(
-                borderRadius:
-                    BorderRadius.circular(8.0), // Dostosuj zaokrąglenie rogów
-                side: const BorderSide(
-                    color: Color.fromARGB(
-                        192, 255, 0, 0)), // Dostosuj kolor obramowania
+                'Powiadomimy Cię, gdy liczba osób\nprzed tobą zmniejszy się do 3.',
+                textAlign: TextAlign.center,
               ),
             ),
-            child: const Text(
-              'Powrót do ekranu głównego',
-              style: TextStyle(
-                color: Color.fromARGB(255, 255, 255, 255),
-                fontSize: 18.0, // Dostosuj rozmiar tekstu
-                fontWeight: FontWeight.bold, // Dostosuj wagę tekstu
+            const SizedBox(
+              height: 20,
+            ),
+            const Center(
+              child: Text(
+                'Udaj się do:\nUrząd Miasta Płocka- Oddział Komunikacji\nAleja marszałka Józefa Piłsudzkiego 6\ntel. 24 364 55 55',
+                textAlign: TextAlign.center,
               ),
             ),
-          ),
-        ]));
+            const SizedBox(
+              height: 20,
+            ),
+            const Center(
+                child: Text(
+              'Twoją sprawą zajmię się:\nJolanta Krysiak Pokój\nnr. 26 (wejście od parkingu)\ntel. +48 909 789 675\nE-mail: jkrysiak87@plock.eu',
+              textAlign: TextAlign.center,
+            )),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Tutaj możesz dodać akcję, która ma być wykonywana po naciśnięciu przycisku.
+                // Na przykład można dodać nawigację do innej strony.
+                Navigator.of(context)
+                    .pushNamedAndRemoveUntil(homePage, (route) => false);
+              },
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size(350, 50),
+                backgroundColor: const Color.fromARGB(173, 244, 18, 18),
+                padding:
+                    const EdgeInsets.all(16.0), // Dostosuj wcięcie przycisku
+                shape: RoundedRectangleBorder(
+                  borderRadius:
+                      BorderRadius.circular(8.0), // Dostosuj zaokrąglenie rogów
+                  side: const BorderSide(
+                      color: Color.fromARGB(
+                          192, 255, 0, 0)), // Dostosuj kolor obramowania
+                ),
+              ),
+              child: const Text(
+                'Powrót do ekranu głównego',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 255, 255, 255),
+                  fontSize: 18.0, // Dostosuj rozmiar tekstu
+                  fontWeight: FontWeight.bold, // Dostosuj wagę tekstu
+                ),
+              ),
+            ),
+          ]),
+        ));
   }
 }
