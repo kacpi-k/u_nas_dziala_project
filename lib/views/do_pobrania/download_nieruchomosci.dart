b61221bee4e86c133dc01580e9160817ccc56eca
kacperpasikowski <pasikowskik@wp.pl>
2023-10-08 10:17:27 +0200

    joooooo

---
+++ lib/views/do_pobrania/download_nieruchomosci.dart
@@ -0,0 +1,178 @@
+import 'package:flutter/material.dart';
+import 'package:u_nas_dziala_project/views/pojazd/brak_dowodu.dart';
+import 'package:u_nas_dziala_project/views/pojazd/brak_karty.dart';
+
+class NieruchomosciDownload extends StatefulWidget {
+  const NieruchomosciDownload({super.key});
+
+  @override
+  State<NieruchomosciDownload> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<NieruchomosciDownload> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        //backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Center(
+          child: Text('Mobilny Informator'),
+        ),
+      ),
+      body: Column(
+        children: [
+          Align(
+            alignment: Alignment.center,
+            child: Image.asset('icons/herb_icon.png'),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Text(
+            style: TextStyle(fontSize: 24),
+            'Dokumenty do pobrania',
+          ),
+          const SizedBox(
+            height: 80,
+          ),
+          const Text(
+            style: TextStyle(fontSize: 24),
+            'Nieruchomości',
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const BrakKartyPojazd()));
+              // Tutaj możesz dodać akcję, która ma być wykonywana po naciśnięciu przycisku.
+              // Na przykład można dodać nawigację do innej strony.
+            },
+            style: ElevatedButton.styleFrom(
+              minimumSize: const Size(350, 50),
+              backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+              padding: const EdgeInsets.all(16.0), // Dostosuj wcięcie przycisku
+              shape: RoundedRectangleBorder(
+                borderRadius:
+                    BorderRadius.circular(8.0), // Dostosuj zaokrąglenie rogów
+                side: const BorderSide(
+                    color: Color.fromARGB(
+                        255, 193, 193, 193)), // Dostosuj kolor obramowania
+              ),
+            ),
+            child: const Text(
+              'Formularz IN-1',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 18.0, // Dostosuj rozmiar tekstu
+                fontWeight: FontWeight.bold, // Dostosuj wagę tekstu
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const BrakKartyPojazd()));
+              // Tutaj możesz dodać akcję, która ma być wykonywana po naciśnięciu przycisku.
+              // Na przykład można dodać nawigację do innej strony.
+            },
+            style: ElevatedButton.styleFrom(
+              minimumSize: const Size(350, 50),
+              backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+              padding: const EdgeInsets.all(16.0), // Dostosuj wcięcie przycisku
+              shape: RoundedRectangleBorder(
+                borderRadius:
+                    BorderRadius.circular(8.0), // Dostosuj zaokrąglenie rogów
+                side: const BorderSide(
+                    color: Color.fromARGB(
+                        255, 193, 193, 193)), // Dostosuj kolor obramowania
+              ),
+            ),
+            child: const Text(
+              'Formularz ZIN-1',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 18.0, // Dostosuj rozmiar tekstu
+                fontWeight: FontWeight.bold, // Dostosuj wagę tekstu
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const BrakKartyPojazd()));
+              // Tutaj możesz dodać akcję, która ma być wykonywana po naciśnięciu przycisku.
+              // Na przykład można dodać nawigację do innej strony.
+            },
+            style: ElevatedButton.styleFrom(
+              minimumSize: const Size(350, 50),
+              backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+              padding: const EdgeInsets.all(16.0), // Dostosuj wcięcie przycisku
+              shape: RoundedRectangleBorder(
+                borderRadius:
+                    BorderRadius.circular(8.0), // Dostosuj zaokrąglenie rogów
+                side: const BorderSide(
+                    color: Color.fromARGB(
+                        255, 193, 193, 193)), // Dostosuj kolor obramowania
+              ),
+            ),
+            child: const Text(
+              'Formularz ZIN-2',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 18.0, // Dostosuj rozmiar tekstu
+                fontWeight: FontWeight.bold, // Dostosuj wagę tekstu
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const BrakKartyPojazd()));
+              // Tutaj możesz dodać akcję, która ma być wykonywana po naciśnięciu przycisku.
+              // Na przykład można dodać nawigację do innej strony.
+            },
+            style: ElevatedButton.styleFrom(
+              minimumSize: const Size(350, 50),
+              backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+              padding: const EdgeInsets.all(16.0), // Dostosuj wcięcie przycisku
+              shape: RoundedRectangleBorder(
+                borderRadius:
+                    BorderRadius.circular(8.0), // Dostosuj zaokrąglenie rogów
+                side: const BorderSide(
+                    color: Color.fromARGB(
+                        255, 193, 193, 193)), // Dostosuj kolor obramowania
+              ),
+            ),
+            child: const Text(
+              'Formularz ZIN-3',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 18.0, // Dostosuj rozmiar tekstu
+                fontWeight: FontWeight.bold, // Dostosuj wagę tekstu
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
